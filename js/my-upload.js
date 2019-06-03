08a8cff1138e4b14993368bca1761e7ace887a14
Eli Donahue <edonahue@aras.com>
2019-06-03 16:22:59 -0400

    Update comments and cleanup

--- js/my-upload.js
+++ js/my-upload.js
@@ -1,7 +1,8 @@
-/**** main function **********************************************************/
-
 /**
- * 
+ * This is the main function of the rest-upload-example project. 
+ * When the user clicks "submit" on the main form, this function retrieves 
+ * the user's input, validates their input, and then uploads the selected 
+ * file to the Aras Innovator vault.
  */
 function submitForm() {
     // get and validate the connection input provided by the end user
@@ -25,17 +26,17 @@ function submitForm() {
 
     var transaction_id = "";
     var file_id = generateNewGuid();
-    console.log("file id: " + file_id);
 
     // get transactionid
     var transaction = httpReq("POST", transactionUrl(creds.url), auth_headers);
     transaction.then(function (transaction_req) {
-        transaction_id = getTransactionIdFromResponse(transaction_req);
-        return transaction_id;
+        var response_object = JSON.parse(
+            transaction_req.responseText.toString()
+        );
+        transaction_id = response_object.transactionId;
 
     }).then(function () {
-        // upload the file chunk(s)
-        console.log("transaction id: " + transaction_id);
+        // upload the file
         return uploadFile(my_file, file_id, transaction_id, creds, auth_headers);
 
     });
@@ -45,13 +46,13 @@ function submitForm() {
 /**** request helpers ********************************************************/
 
 /**
- * A helper function to send an HTTP call to the specified url.
+ * Sends an HTTP call to the specified url.
  *
  * @param {*} type - HTTP call type. (GET or POST for this sample project)
  * @param {*} url - url to use for the HTTP call
  * @param {*} headers - an array of objects representing headers. Ex: [{name: "Content-Type", value: "application/json"}, {name: "AUTHUSER", value: "admin"}]
  * @param {*} body - body content of the HTTP call
- * @param {*} attempts - number of times we should attempt this call. Default value is 0.
+ * @param {*} attempts - number of times we should attempt this call. Default value is 0. If attempts = 0, the call will be made synchronously.
  */
 function httpReq(type, url, headers, body, attempts = 0) {
     return new Promise(function (resolve, reject) {
@@ -63,8 +64,6 @@ function httpReq(type, url, headers, body, attempts = 0) {
             httpRequest.open(type, url, false);
         }
 
-        console.log(type + " : " + url);
-
         // add any headers passed to function
         headers = headers || [];
         for (var i = 0; i < headers.length; i++) {
@@ -75,30 +74,35 @@ function httpReq(type, url, headers, body, attempts = 0) {
         // send the request
         httpRequest.send(body);
 
-        // return httpRequest;
-
         // resolve or reject the promise when the response comes back
         httpRequest.onreadystatechange = function () {
             if (httpRequest.readyState == 4 && httpRequest.status == 200) {
                 // resolve the promise and return the request object
-                console.log("resolved : " + httpRequest.responseText.toString());
                 resolve(httpRequest);
 
             } else if (httpRequest.readyState == 4 && attempts !== 0) {
                 // we have more attempts left, retry
-                console.log("retrying : " + httpRequest.responseText.toString());
                 return httpReq(type, url, headers, body, attempts - 1);
 
             } else if (httpRequest.readyState == 4 && httpRequest.status == 400) {
                 // reject the promise and return an error
-                console.log("rejecting : " + httpRequest.responseText.toString());
-                var err = new Error(httpRequest.statusText);
-                reject(err);
+                reject(new Error(httpRequest.statusText));
             }
         };
     });
 }
 
+/**
+ * Uploads the user's file to the vault server, breaking it into smaller chunks if necessary. 
+ * When the upload is finished, we commit the transaction before returning.
+ * 
+ * @param {*} file - the file object to upload
+ * @param {*} file_id - the id for the new File item in Aras
+ * @param {*} transaction_id - the transaction_id for the vault server
+ * @param {*} creds - an object containing the user's connection info (url, database, etc.)
+ * @param {*} auth_headers - an array of objects representing the headers required for an Innovator REST call
+ * @param {*} chunk_size - the max size of file content to upload in one call (bytes). Default value is 10,000. If the file is larger than chunk_size, we'll break up the file and send it in multiple requests.
+ */
 function uploadFile(file, file_id, transaction_id, creds, auth_headers, chunk_size = 10000) {
     var upload_url = fileUploadUrl(creds.url, file_id);
     var size = file.size;
@@ -107,20 +111,22 @@ function uploadFile(file, file_id, transaction_id, creds, auth_headers, chunk_si
 
     setTimeout(loop, 1);
 
+    // we need to upload the file chunk(s) sequentially 
     function loop() {
         var end = start + chunk_size;
         if (size - end < 0) {
             end = size;
         }
-
-        // var chunk = slice(file, start, end);
         var chunk = file.slice(start, end);
 
         // get an array of headers for this upload request
         var headers = getUploadHeaders(auth_headers, escapeURL(file.name), start, end - 1, size, transaction_id);
 
+        // make the request to upload this file content
         httpReq("POST", upload_url, headers, chunk, attempts);
 
+        // if there is still content left to upload, update the start and loop again
+        // otherwise, we'll close out this transaction with a commit request
         if (end < size - 1) {
             start += chunk_size;
             setTimeout(loop, 1);
@@ -130,30 +136,42 @@ function uploadFile(file, file_id, transaction_id, creds, auth_headers, chunk_si
     }
 }
 
-function commitTransaction(my_file, file_id, transaction_id, creds, auth_headers) {
+/**
+ * Makes the final call to commit the file upload transaction to the vault server.
+ * 
+ * @param {*} file - the file object to upload
+ * @param {*} file_id - the id for the new File item in Aras
+ * @param {*} transaction_id - the transaction_id for the vault server
+ * @param {*} creds - an object containing the user's connection info (url, database, etc.)
+ * @param {*} auth_headers - an array of objects representing the headers required for an Innovator REST call
+ */
+function commitTransaction(file, file_id, transaction_id, creds, auth_headers) {
+    // build the headers and body for the commit request
     var boundary = "batch_" + file_id;
     var commit_headers = getCommitHeaders(boundary, transaction_id, auth_headers);
-    var commit_body = getCommitBody(boundary, creds, file_id, my_file);
+    var commit_body = buildCommit(boundary, creds, file_id, file);
 
+    // send the commit request to the vault server
     var commit = httpReq("POST", commitUrl(creds.url), commit_headers, commit_body);
     commit.then(function (commit_response) {
         var response = commit_response.responseText.toString();
         var response_json = response.substring(response.indexOf("{"), response.lastIndexOf("}") + 1);
-        console.log(response_json);
         var response_object = JSON.parse(response_json);
+
+        // inform the user that the upload and commit were successful
         alert("Successfully uploaded file '" + response_object.filename + "' with id '" + response_object.id + "'");
     });
 }
 
 /**
+ * Returns a specially formatted string for the body of a transaction commit call. 
  * 
- * 
- * @param {*} boundary 
- * @param {*} creds 
- * @param {*} file_id 
- * @param {*} my_file 
+ * @param {*} boundary - a specific string for marking the beginning and end of commit content
+ * @param {*} creds - an object containing the user's connection info (url, database, etc.)
+ * @param {*} file_id - the id for the new File item in Aras
+ * @param {*} file - the file object to upload
  */
-function getCommitBody(boundary, creds, file_id, my_file) {
+function buildCommit(boundary, creds, file_id, file) {
     // it's important to use the \r\n end of line character, otherwise commit will fail
     var EOL = "\r\n";
 
@@ -170,8 +188,8 @@ function getCommitBody(boundary, creds, file_id, my_file) {
     commit_body += EOL;
     commit_body += EOL;
     commit_body += '{"id":"' + file_id + '",';
-    commit_body += '"filename":"' + my_file.name + '",';
-    commit_body += '"file_size":' + my_file.size + ',';
+    commit_body += '"filename":"' + file.name + '",';
+    commit_body += '"file_size":' + file.size + ',';
     commit_body += '"Located":[{"file_version":1,"related_id":"67BBB9204FE84A8981ED8313049BA06C"}]}';
     commit_body += EOL;
     commit_body += "--" + boundary + "--";
@@ -179,49 +197,42 @@ function getCommitBody(boundary, creds, file_id, my_file) {
     return commit_body;
 }
 
-/**
- * 
- * @param {*} transaction_response 
- */
-function getTransactionIdFromResponse(transaction_response) {
-    var response_object = JSON.parse(
-        transaction_response.responseText.toString()
-    );
-    return response_object.transactionId;
-}
-
 
 /**** url helpers ************************************************************/
 
 /**
+ * Returns the url used to start a vault server transaction
  * 
- * @param {*} server_url 
+ * @param {*} server_url - url of the Aras Innovator application server
  */
 function transactionUrl(server_url) {
     return server_url + "/vault/odata/vault.BeginTransaction";
 }
 
 /**
+ * Returns the url used to upload the specified file to the vault server
  * 
- * @param {*} server_url 
- * @param {*} file_id 
+ * @param {*} server_url - url of the Aras Innovator application server
+ * @param {*} file_id - the id for the new File item in Aras
  */
 function fileUploadUrl(server_url, file_id) {
     return server_url + "/vault/odata/vault.UploadFile?fileId=" + file_id;
 }
 
 /**
+ * Returns the url used to commit a vault server transaction
  * 
- * @param {*} server_url 
+ * @param {*} server_url - url of the Aras Innovator application server
  */
 function commitUrl(server_url) {
     return server_url + "/vault/odata/vault.CommitTransaction";
 }
 
 /**
+ * Returns the url used to retrieve the specified file from Aras Innovator
  * 
- * @param {*} server_url 
- * @param {*} file_id 
+ * @param {*} server_url - url of the Aras Innovator application server
+ * @param {*} file_id - the id for the new File item in Aras
  */
 function getFileUrl(server_url, file_id) {
     return server_url + "/server/odata/file('" + file_id + "')";
@@ -231,7 +242,7 @@ function getFileUrl(server_url, file_id) {
 /**** header helpers *********************************************************/
 
 /**
- * Returns an array of objects representing the headers required for an Innovator REST call using basic authentication (password rather than OAuth token).
+ * Returns an array of objects representing the headers required for an Innovator REST call using basic authentication (password rather than OAuth token)
  * 
  * @param {*} creds - an object containing the connection data provided by the end user. See getConnectionInput for an example.
  */
@@ -254,13 +265,14 @@ function getBasicAuthHeaders(creds) {
 }
 
 /**
+ * Returns an array of objects representing the headers required to upload a file to the vault server
  * 
- * @param {*} auth_headers 
- * @param {*} escaped_name 
- * @param {*} start_range 
- * @param {*} end_range 
- * @param {*} file_size 
- * @param {*} transaction_id 
+ * @param {*} auth_headers - an array of objects representing the headers required for an Innovator REST call
+ * @param {*} escaped_name - the name of the file to upload, with special characters encoded (ex. spaces replaced with '%20')
+ * @param {*} start_range - the first index of the file chunk
+ * @param {*} end_range - the last index of the file chunk
+ * @param {*} file_size - the size of the file chunk
+ * @param {*} transaction_id - the transaction id for this upload request
  */
 function getUploadHeaders(auth_headers, escaped_name, start_range, end_range, file_size, transaction_id) {
     // start the upload headers array with a clone of the auth_headers array
@@ -288,10 +300,11 @@ function getUploadHeaders(auth_headers, escaped_name, start_range, end_range, fi
 }
 
 /**
+ * Returns an array of objects representing the headers required to commit a vault server transaction
  * 
- * @param {*} boundary 
- * @param {*} transaction_id 
- * @param {*} auth_headers 
+ * @param {*} boundary - a specific string for marking the beginning and end of commit content
+ * @param {*} transaction_id - the transaction id for this upload request
+ * @param {*} auth_headers - an array of objects representing the headers required for an Innovator REST call
  */
 function getCommitHeaders(boundary, transaction_id, auth_headers) {
     var commit_headers = [];
@@ -378,7 +391,7 @@ function getFileInput() {
 /**** misc utilities *********************************************************/
 
 /**
- * 
+ * Returns a new 32 character GUID we can use as an Aras Item id
  */
 function generateNewGuid() {
     function randomDigit() {
@@ -395,8 +408,9 @@ function generateNewGuid() {
 }
 
 /**
- * escapes the following characters: %, ' ', ', !, ", #, $, &, (, ), *, +, ?
- * @param {*} url 
+ * Escapes the following characters in a url: %, ' ', ', !, ", #, $, &, (, ), *, +, ?
+ * 
+ * @param {*} url - the url string we need to escape
  */
 function escapeURL(url) {
     url = url.split('%').join('%25');
